03dab0e0652d34335ca3dfb78e21e6d71c7bc3ad
secustor <sebastian@poxhofer.at>
2021-07-18 21:33:38 +0200

    init

---
+++ subfolder/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "aws"
+      version = "3.0.0"
+    }
+    azurerm = {
+      source  = "azurerm"
+      version = "~> 2.50"
+    }
+    random = {
+      version = "~> 2.2"
+    }
+    proxmox = {
+      source = "Telmate/proxmox"
+      version = "~> 2.6.1"
+    }
+  }
+}
